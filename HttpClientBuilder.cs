7d7910f73576c3d7c1f12ed347a97d2b95281e54
Troy Crowe <troy.crowe@live.com>
2025-10-18 12:22:17 -0500

    Added new endpoints

--- HttpClientBuilder.cs
+++ HttpClientBuilder.cs
@@ -4,7 +4,6 @@
     public class HttpClientBuilder : Interfaces.IhttpClientBuilder
     {
         private readonly IHttpClientFactory _clientFactory;
-        private HttpMessageHandler handler;
 
         public HttpClientBuilder(IHttpClientFactory httpClientFactory)
         {
